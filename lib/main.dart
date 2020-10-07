b3c9d9c665898ac4b44cecb7280c832671618268
Esteban Boucher <es.boucher@protomail.com>
2020-10-07 23:40:47 +0200

    Testing colors & fonts

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,24 @@ void main() {
       appBar: AppBar(
         title: Text('Co\'Work'),
         centerTitle: true,
+        backgroundColor: Colors.blueAccent[200],
       ),
       body: Center(
-        child: Text('Пиздун ебаный чтобы ты хуй на жопу закрипил ебан трёхдневный.'),
+        child: Text(
+          'Пиздун ебаный чтобы ты хуй на жопу закрипил ебан трёхдневный.',
+          style: TextStyle(
+            fontSize: 20.0,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2.0,
+            color: Colors.grey[600],
+            fontFamily: 'IndieFlower',
+          ),
+        ),
       ),
       floatingActionButton: FloatingActionButton(
+        onPressed: () {},
         child: Text('Нахуй всё'),
+        backgroundColor: Colors.blueAccent[200],
       ),
     ),
   ));
