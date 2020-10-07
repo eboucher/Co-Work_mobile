d79b93af95fcf8243a94887b0b12ff99106398c6
Esteban Boucher <es.boucher@protomail.com>
2020-10-07 21:48:27 +0200

    Scaffold & AppBar Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,6 +5,13 @@ void main() {
     home: Scaffold(
       appBar: AppBar(
         title: Text('Co\'Work'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('Пиздун ебаный чтобы ты хуй на жопу закрипил ебан трёхдневный.'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('Нахуй всё'),
       ),
     ),
   ));
