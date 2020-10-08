67423c2e16476077c009363a93bb8a8c35c671d5
Esteban Boucher <es.boucher@protomail.com>
2020-10-08 23:25:54 +0200

    A custom class and a card

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
+import 'quote.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: Home(),
-  ));
+void main() => runApp(MaterialApp(
+    home: QuoteList()
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
 }
 
+class _QuoteListState extends State<QuoteList> {
 
-class Home extends StatelessWidget {
+  List<Quote> quotes = [
+    Quote(author: 'Oscar Wilde', text: 'Be yourself; everyone else is already taken'),
+    Quote(author: 'Oscar Wilde', text: 'I have nothing to declare except my genius'),
+    Quote(author: 'Oscar Wilde', text: 'The truth is rarely pure and never simple')
+  ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+        margin: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Text(
+                quote.text,
+                style: TextStyle(
+                  fontSize: 18.0,
+                  color: Colors.grey[600],
+                ),
+              ),
+              SizedBox(height: 6.0),
+              Text(
+                quote.author,
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.grey[800],
+                ),
+              ),
+            ],
+          ),
+        )
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        title: Text('Co\'Work'),
+        title: Text('Awesome Quotes'),
         centerTitle: true,
-        backgroundColor: Colors.blueAccent[200],
+        backgroundColor: Colors.redAccent,
       ),
-      body: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text('Мне пиздец'),
-          FlatButton(
-            onPressed: (){},
-            color: Colors.amber,
-            child: Text('Всем похуй'),
-          ),
-          Container(
-            color: Colors.cyan,
-            padding: EdgeInsets.all(30.0),
-            child: Text('Ебанута какая-то'),
-          ),
-        ],
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Text('Нахуй всё'),
-        backgroundColor: Colors.blueAccent[200],
+      body: Column(
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
