25c2eb68ee17d15f49cf49b5ba6232994667b5ca
Esteban Boucher <es.boucher@protomail.com>
2020-10-09 03:45:03 +0200

    Getting started with authentication

---
+++ lib/screens/login.dart
@@ -0,0 +1,19 @@
+import 'package:cowork_mobile/screens/register.dart';
+import 'package:cowork_mobile/services/auth.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'home.dart';
+
+
+class Login extends StatefulWidget {
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
