0e30a511c7ce7858bd3bfe0224dc86fe2b79380f
Esteban Boucher <es.boucher@protomail.com>
2020-10-12 14:16:30 +0200

    Working on home screen after login redirection

---
+++ lib/screens/booking.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+
+class Booking extends StatefulWidget {
+  @override
+  _BookingState createState() => _BookingState();
+}
+
+class _BookingState extends State<Booking> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:cowork_mobile/screens/booking.dart';
+import 'package:cowork_mobile/screens/bookings.dart';
+import 'package:cowork_mobile/helpers/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 
@@ -10,35 +15,68 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( backgroundColor: Color(0xFF283593),title: Text(title)),
-      body:
+        appBar: AppBar( backgroundColor: Color(0xFF283593),title: Text(title)),
+        body:
         Container(
-          child:
+            child:
             SingleChildScrollView()),
 
-      drawer: Container(
-        width:200,
-        child:Drawer(
-          // Add a ListView to the drawer. This ensures the user can scroll
-          // through the options in the drawer if there isn't enough vertical
-          // space to fit everything.
-          child: ListView(
-            // Important: Remove any padding from the ListView.
-            padding: EdgeInsets.zero,
-            children: <Widget>[
-              Container(
-                height: 85.0,
-                child:DrawerHeader(
-                  child: Text('Co\'Work'),
-                  decoration: BoxDecoration(
-                    color: Color(0xFF283593),
+        drawer: Container(
+          width:200,
+          child:Drawer(
+            // Add a ListView to the drawer. This ensures the user can scroll
+            // through the options in the drawer if there isn't enough vertical
+            // space to fit everything.
+            child: ListView(
+              // Important: Remove any padding from the ListView.
+              padding: EdgeInsets.zero,
+              children: <Widget>[
+                Container(
+                  height: 85.0,
+                  child:DrawerHeader(
+                    child: Text('Co\'Work'),
+                    decoration: BoxDecoration(
+                      color: Color(0xFF283593),
+                    ),
                   ),
                 ),
-              )
-            ],
+                ListTile(
+                  title: Text('Book room'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    print('eee');
+                    Navigator.push(  context,
+                      MaterialPageRoute(builder: (context) => Booking()),);
+                    // Then close the drawer
+                    //Navigator.pop(context);
+
+                  },
+                ),
+                ListTile(
+                  title: Text('My bookings'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.push(  context,
+                      MaterialPageRoute(builder: (context) => Bookings()),);
+                    //Navigator.pop(context);
+                  },
+                ),
+                ListTile(
+                  title: Text('My account'),
+                  onTap: () {
+                    // Update the state of the app
+                    // ...
+                    // Then close the drawer
+                    Navigator.pop(context);
+                  },
+                ),
+              ],
+            ),
           ),
-        ),
-      )
+        )
     );
   }
 }
