f864e8d39dc4251fae8da35d25dee999e8621988
Esteban Boucher <es.boucher@protomail.com>
2020-10-13 21:59:53 +0200

    Trying to do a list of cards in home screen

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -21,6 +21,37 @@ class Home extends StatelessWidget {
   User user;
   Home({Key key, this.title, this.user}) : super(key: key);
 
+  Widget _buildList() => ListView(
+    children: [
+      _tile('CineArts at the Empire', '85 W Portal Ave', Icons.theaters),
+      _tile('The Castro Theater', '429 Castro St', Icons.theaters),
+      _tile('Alamo Drafthouse Cinema', '2550 Mission St', Icons.theaters),
+      _tile('Roxie Theater', '3117 16th St', Icons.theaters),
+      _tile('United Artists Stonestown Twin', '501 Buckingham Way',
+          Icons.theaters),
+      _tile('AMC Metreon 16', '135 4th St #3000', Icons.theaters),
+      Divider(),
+      _tile('K\'s Kitchen', '757 Monterey Blvd', Icons.restaurant),
+      _tile('Emmy\'s Restaurant', '1923 Ocean Ave', Icons.restaurant),
+      _tile(
+          'Chaiya Thai Restaurant', '272 Claremont Blvd', Icons.restaurant),
+      _tile('La Ciccia', '291 30th St', Icons.restaurant),
+    ],
+  );
+
+  ListTile _tile(String title, String subtitle, IconData icon) => ListTile(
+    title: Text(title,
+        style: TextStyle(
+          fontWeight: FontWeight.w500,
+          fontSize: 20,
+        )),
+    subtitle: Text(subtitle),
+    leading: Icon(
+      icon,
+      color: Colors.blue[500],
+    ),
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,6 +62,8 @@ class Home extends StatelessWidget {
             SingleChildScrollView(
               child:
               Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
                 children: <Widget>[
                   Text(
                     "Co'Work",
@@ -61,7 +94,23 @@ class Home extends StatelessWidget {
                         fontWeight: FontWeight.w300
                       )
                     ),
-                  )
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(20.0),
+                    color: Colors.cyan,
+                    child: Text('one'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(30.0),
+                    color: Colors.pinkAccent,
+                    child: Text('two'),
+                  ),
+                  Container(
+                    padding: EdgeInsets.all(40.0),
+                    color: Colors.amber,
+                    child: Text('three'),
+                  ),
+                  _buildList()
                 ],
               ),
             )
